6c19218e83bab172d2514d1c7a488e79dd780431
thakurdivendra <dkghlaut111@gmail.com>
2017-04-01 17:35:26 +0530

    design compelete

--- js/custom.js
+++ js/custom.js
@@ -5,6 +5,9 @@ $(function() {
 			/*
 			* Plugin intialization
 			*/
+			if(window.outerWidth<=600){
+
+			}else{
 	    	$('#pagepiling').pagepiling({
 	    		menu: '#menu',
 	    		anchors: ['page1', 'page2', 'page3', 'page4'],
@@ -19,15 +22,81 @@ $(function() {
 			    afterLoad: function(anchorLink, index){
 			    	if(index>1){
 			    		$('#pp-nav').removeClass('custom');
+			    		if(anchorLink=='page1'){
+			    			$('header.header').removeClass('fix');
+			    		}else if($('header.header').hasClass('fix')){
+			    		}else{
+			    			$('header.header').addClass('fix')
+			    		}
 			    	}else{
 			    		$('#pp-nav').addClass('custom');
+			    		if(anchorLink=='page1'){
+			    			$('header.header').removeClass('fix');
+			    		}else if($('header.header').hasClass('fix')){
+			    		}else{
+			    			$('header.header').addClass('fix')
+			    		}
 			    	}
 			    }
 			});
+	    }
 		});
 $('ul.tabs > li').click(function(){
             $('div.tab-content').find('div.active-content').removeClass('active-content');
             $('ul.tabs > li').removeClass('active');
             $(this).parents('div.tabs-sec').find('div.tab-content').find('div.'+this.className).addClass('active-content');
             $(this).addClass('active');
+        });
+$(function(){
+		$('div.section').css('padding-top',$('header.header').outerHeight()-10)
+});
+$(function(){
+	$("#testomanial-slider").lightSlider({
+          gallery:true,
+          item:1,
+          thumbItem:0,
+          slideMargin: 0,
+          speed:1200,
+          auto:true,
+          loop:true,
+          onSliderLoad: function() {
+              $('#testomanial-slider').removeClass('cS-hidden');
+          }
+      });
+	$("#pfq-slider").lightSlider({
+          gallery:true,
+          item:3,
+          thumbItem:0,
+          slideMargin: 10,
+          speed:1200,
+          auto:true,
+          loop:true,
+          onSliderLoad: function() {
+              $('#pfq-slider').removeClass('cS-hidden');
+          }
+      });
+})
+$(function() {
+        $('ul.tabs > li').click(function(){
+            $('div.tab-content').find('div.active-content').removeClass('active-content');
+            $('ul.tabs > li').removeClass('active');
+            $(this).parents('div.tabs-sec').find('div.tab-content').find('div.'+this.className).addClass('active-content');
+            $(this).addClass('active');
         })
+        $('.panel > .panel-title').click(function(){
+            if($(this).parents('.panel').find('.panel-content').hasClass('in')==true){
+                $(this).parents('.panel').find('.panel-content').slideUp(150).removeClass('in');
+                $(this).find('i.fa').removeClass('fa-minus').addClass('fa-angle-down');
+            }else if($('.panel-content').hasClass('in')==true){
+                $('.faq-content .panel-content').removeClass('in').slideUp(150);
+                $('.panel-title').find('i.fa').removeClass('fa-minus').addClass('fa-angle-down');
+                $(this).parents('.panel').find('.panel-content').slideDown(150).addClass('in');
+                $(this).find('i.fa').removeClass('fa-angle-down').addClass('fa-minus');
+            }else{
+            $(this).parents('.panel').find('.panel-content').slideDown(150).addClass('in');
+            $(this).find('i.fa').removeClass('fa-angle-down').addClass('fa-minus')
+            }
+        });
+
+        $('.panel-content').slideUp().eq(0).slideDown(150).addClass('in').parents('.panel').find('.panel-title').find('i.fa').removeClass('fa-angle-down').addClass('fa-minus');
+    });
